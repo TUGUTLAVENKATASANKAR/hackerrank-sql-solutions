4da8042578a6c3fad684a13e7f88c216331a318c
TUGUTLA VENKATA SANKR <vstugutla1@gmail.com>
2026-01-20 11:17:27 +0530

    Weather_Observation_Station_1.sql
    Added solution for Weather Observation Station 1

---
+++ SQL/Weather_Observation_Station_1.sql
@@ -0,0 +1,8 @@
+-- Platform: HackerRank
+-- Problem: Weather Observation Station 1
+-- Difficulty: Easy
+-- Description:
+-- Query a list of CITY and STATE from the STATION table.
+
+SELECT CITY, STATE
+FROM STATION
