b752ffd4ae3a8dd9bbb3ce5fe1aa3577da9c8e30
TUGUTLA VENKATA SANKR <vstugutla1@gmail.com>
2026-01-20 16:25:07 +0530

    Added SQL Server solution for Select By ID

---
+++ SQL/Select_By_ID_SQLServer.sql
@@ -0,0 +1,9 @@
+-- Platform: HackerRank
+-- Problem: Select By ID
+-- SQL Dialect: SQL Server
+-- Description:
+-- Query all columns for a city in CITY with the ID 1661.
+
+SELECT *
+FROM CITY
+WHERE ID = 1661
