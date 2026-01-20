3a57be9629c89bebfdb92b693fd994c67dafe715
TUGUTLA VENKATA SANKR <vstugutla1@gmail.com>
2026-01-20 16:38:21 +0530

    Added SQL Server solution for Employee Names problem

---
+++ SQL/Employee_Names_SQLServer.sql
@@ -0,0 +1,11 @@
+
+-- Platform: HackerRank
+-- Problem: Employee Names
+-- SQL Dialect: SQL Server
+-- Description:
+-- Write a query that prints a list of employee names from the Employee table
+-- in alphabetical order.
+
+SELECT NAME
+FROM EMPLOYEE
+ORDER BY NAME
