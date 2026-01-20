f2e3432febcef1345c98ea4aa60e4021e746c2c2
TUGUTLAVENKATASANKAR <vstugutla1@gmail.com>
2026-01-20 10:57:56 +0530

    Revising_Select_Query_II.sql
    Added solution for Revising the Select Query II

---
+++ SQL/Revising_Select_Query_II.sql
@@ -0,0 +1,14 @@
+-- Platform: HackerRank
+-- Problem: Revising the Select Query II
+-- Difficulty: Easy
+-- Description:
+-- Query all columns for all American cities in the CITY table with populations larger than 100000.
+-- CountryCode for America is USA.
+
+SELECT *
+FROM CITY
+WHERE COUNTRYCODE = 'USA'
+  AND POPULATION > 100000;
+
+
+
