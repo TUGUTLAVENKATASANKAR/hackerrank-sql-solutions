2beb2f02d4244b962b1a0040f4f8ddd9b5593cad
TUGUTLA VENKATA SANKR <vstugutla1@gmail.com>
2026-01-22 12:13:08 +0530

    Added SQL Server solution for Weather Observation Station 17

---
+++ SQL/Weather_Observation_Station_17_SQLServer.sql
@@ -0,0 +1,15 @@
+
+-- Platform: HackerRank
+-- Problem: Weather Observation Station 17
+-- SQL Dialect: SQL Server
+-- Description:
+-- Query the LONG_W for the smallest LAT_N greater than 38.7780
+-- and round the result to 4 decimal places.
+
+SELECT CAST(ROUND(LONG_W, 4) AS DECIMAL(10,4)) AS LONG_W
+FROM STATION
+WHERE LAT_N = (
+    SELECT MIN(LAT_N)
+    FROM STATION
+    WHERE LAT_N > 38.7780
+)
